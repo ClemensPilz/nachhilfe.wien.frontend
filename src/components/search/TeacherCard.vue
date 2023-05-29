ecd6b8491ded9416ca63c91673eac0f8af51b808
Clemens Pilz <office@clemenspilz.com>
2023-05-29 18:40:24 +0200

    added some cards to SearchView

---
+++ src/components/search/TeacherCard.vue
@@ -0,0 +1,46 @@
+<template>
+
+  <!--Card Body-->
+    <div class="p-4 border-b-8 rounded-l-xl shadow-xl border-gray-400
+      flex flex-col">
+        <div class="text-xl text-mainBlue font-bold">Max Mustermann</div>
+        <div class="text-gray-700">Mustermaxl</div>
+        <div class="flex flex-wrap space-x-2 text-xs text-white my-2">
+            <div class="p-1 bg-gray-500 border rounded border-gray-700">Mathematik</div>
+            <div class="p-1 bg-gray-500 border rounded border-gray-700">Englisch</div>
+            <div class="p-1 bg-gray-500 border rounded border-gray-700">Deutsch</div>
+        </div>
+        <div class="flex justify-between items-center">
+            <div class="text-xs text-gray-700">1010 Favoriten</div>
+            <div class="text-3xl text-mainBlue font-bold font-serif">21 €</div>
+        </div>
+    </div>
+  <!--Divider-->
+    <div class="h-20"></div>
+
+  <!--Card Body 2-->
+    <div class="p-4 rounded-xl border-mainBlue shadow-xl
+      flex flex-col">
+        <div class="text-xl text-mainBlue font-bold">Max Mustermann</div>
+        <div class="text-gray-700 uppercase italic">Mustermaxl</div>
+        <div class="flex flex-wrap space-x-2 text-xs text-white my-2">
+            <div class="p-1 bg-gray-500 border rounded border-gray-700">Mathematik</div>
+            <div class="p-1 bg-gray-500 border rounded border-gray-700">Englisch</div>
+            <div class="p-1 bg-gray-500 border rounded border-gray-700">Deutsch</div>
+        </div>
+        <div class="flex justify-between items-center">
+            <div class="text-xs text-mainBlue">1010 Favoriten</div>
+            <div class="text-3xl text-yellow-400 font-bold font-serif">21 €</div>
+        </div>
+    </div>
+
+
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,11 +1,12 @@
 <template>
-    <div>
-
+    <div class="container min-h-screen flex flex-col items-center justify-center bg-white">
+        <TeacherCard />
     </div>
 </template>
 
 <script setup>
 
+import TeacherCard from "@/components/search/TeacherCard.vue";
 </script>
 
 <style lang="scss" scoped>
