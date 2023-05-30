ea1ffc662165f2e4ef3f8ab461e2635fda1bf626
Clemens Pilz <office@clemenspilz.com>
2023-05-30 09:13:23 +0200

    new card design

--- src/components/search/TeacherCard.vue
+++ src/components/search/TeacherCard.vue
@@ -11,7 +11,7 @@
             <div class="p-1 bg-gray-500 border rounded border-gray-700">Deutsch</div>
         </div>
         <div class="flex justify-between items-center">
-            <div class="text-xs text-gray-700">1010 Favoriten</div>
+            <div class="text-xs text-gray-700">1100 Favoriten: 3,4/5</div>
             <div class="text-3xl text-mainBlue font-bold font-serif">21 €</div>
         </div>
     </div>
@@ -29,11 +29,30 @@
             <div class="p-1 bg-gray-500 border rounded border-gray-700">Deutsch</div>
         </div>
         <div class="flex justify-between items-center">
-            <div class="text-xs text-mainBlue">1010 Favoriten</div>
+            <div class="text-xs text-mainBlue">1100 Favoriten: 3,4/5</div>
             <div class="text-3xl text-yellow-400 font-bold font-serif">21 €</div>
         </div>
     </div>
 
+
+  <!--Divider-->
+    <div class="h-20"></div>
+
+  <!--Card Body 3-->
+    <div class="p-4 border-b-8 rounded-xl shadow-lg shadow-gray-400 border-teal-900 bg-teal-600
+      flex flex-col">
+        <div class="text-xl text-teal-100 font-bold">Max Mustermann</div>
+        <div class="text-teal-200 italic mt-1">Mustermaxl <span class="font-bold text-xs text-teal-200 p-1 px-2 rounded-xl bg-teal-800">3,4</span></div>
+        <div class="flex flex-wrap space-x-2 text-xs text-teal-200 my-2">
+            <div class="p-1 px-2 bg-teal-800 rounded">Mathematik</div>
+            <div class="p-1 px-2 bg-teal-800 rounded">Englisch</div>
+            <div class="p-1 px-2 bg-teal-800 rounded">Deutsch</div>
+        </div>
+        <div class="flex justify-between items-center space-x-2">
+            <div class="text-xs text-teal-200 italic">1100 <br>Wien-Favoriten</div>
+            <div class="text-3xl text-teal-800 font-bold font-serif rounded-2xl p-1 px-2 bg-teal-100">21€</div>
+        </div>
+    </div>
 
 </template>
 
