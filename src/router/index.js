980e9fa754f6ff0ee35d3c1961aaab924ad2afdb
René-Ali Günürün <renee.guenueruen@gmail.com>
2023-06-02 23:59:19 +0200

    added NotesView for personal notes

--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,46 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import InboxView from "@/views/InboxView.vue";
 import DashboardView from "@/views/DashboardView.vue";
 import SearchView from "@/views/SearchView.vue";
 import ProfileView from "@/views/ProfileView.vue";
+import NotesView from "@/views/NotesView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/inbox',
-      name: 'inbox',
-      component: InboxView
-    },
-    {
-      path: '/dashboard',
-      name: 'dashboard',
-      component: DashboardView
-    },
-    {
-      path: '/search',
-      name: 'search',
-      component: SearchView
-    },
-    {
-      path: '/profile',
-      name: 'profile',
-      component: ProfileView
-    }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView
+        },
+        {
+            path: '/inbox',
+            name: 'inbox',
+            component: InboxView
+        },
+        {
+            path: '/dashboard',
+            name: 'dashboard',
+            component: DashboardView
+        },
+        {
+            path: '/search',
+            name: 'search',
+            component: SearchView
+        },
+        {
+            path: '/profile',
+            name: 'profile',
+            component: ProfileView
+        },
+        {
+            path: '/mynotes',
+            name: 'notes',
+            component: NotesView
+        }
+
+    ]
 })
 
 export default router
