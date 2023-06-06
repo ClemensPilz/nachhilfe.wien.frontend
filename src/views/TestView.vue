aecd24668c0436395fca523eb8ff59ca797c0e6d
Clemens Pilz <office@clemenspilz.com>
2023-06-06 07:10:46 +0200

    basic layout for new profile page implemented

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,98 +1,52 @@
 <template>
-  <div class="container max-w-6xl min-h-full mx-auto">
-    <LoginModal />
 
-    <!--Navbar-->
-    <NavBar>
-      <div class="container mx-auto px-4">
-        <div class="flex space-x-2">
-        <button class="py-2 px-4 text-primary text-xs md:text-sm uppercase" @click="openLoginModal">Login</button>
-        <button class="py-.5 px-3 md:py-2 md:px-4 bg-primary text-white text-xl text-xs md:text-sm rounded-xl uppercase" @click="openRegisterModal">Register</button>
-        </div>
+  <!--Container-->
+  <div class="container max-w-6xl mx-auto text-center mt-20 bg-lightPrimary">
+    <!--TopSection-Container-->
+    <div class="flex flex-col space-y-4 w-full bg-blue-500 md:flex-row md:space-y-0 md:space-x-6 md:justify-around md:items-center">
+
+      <!--Image-->
+      <div class="flex flex-col md:order-2 md:-translate-y-10">
+        <img class="w-full md:w-48 md:rounded-full"
+             src="https://images.unsplash.com/photo-1601233749202-95d04d5b3c00?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=838&q=80"
+             alt="profile picture of a funny guy">
       </div>
-    </NavBar>
-
-    <!--Modal-->
-
-    <RegisterModal :isOpen="registerOpen" title="Neuen Account anlegen" @update:registerOpen="v => registerOpen = v">
-    </RegisterModal>
-    <LoginModal :isOpen="loginOpen" title="Einloggen" @update:loginOpen="v => loginOpen = v">
-    </LoginModal>
-    <!--Top-Jumbo-->
-    <section class="mb-12">
-      <HeroImage/>
-    </section>
-
-    <!--Feature-Block-->
-    <section class="mb-12 md:mx-10">
-      <FeatureBlock/>
-    </section>
-
-    <!--Testimonial-->
-    <section>
-      <TestimonialHorizontal
-          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-              scelerisque diam non nisi semper, et elementum lorem ornare.
-              Maecenas placerat facilisis mollis. Duis sagittis ligula in
-              sodales vehicula."
-          name="Anna Annabauer"
-          description="Studentin"
-          img-source="https://tecdn.b-cdn.net/img/Photos/Avatars/img%20%2810%29.jpg"
-          img-alt="Avatar von Anna Annabauer"/>
-    </section>
-
-    <!--Timeline-->
-    <section class="mb-12 mx-16 lg:mx-44">
-      <TimeLine/>
-    </section>
-
-    <!--Lehrer und Schüler Cards-->
-    <section class="mb-12">
-      <div class="flex flex-col md:flex-row gap-4 md:gap-10">
-        <CardNormal title="Nachhilfe finden" class="text-center md:w1/2"
-                    content="Melde dich jetzt an, um einen Nachhilfelehrer in deiner Umgebung zu finden."
-                    button-text="anmelden" button-bg="bg-accent"/>
-
-        <CardNormal title="Lehrer werden" class="text-center md:w-1/2"
-                    content="Du möchtest dein Wissen weitergeben und selbst Lehrer werden? Kontaktiere uns über das Kontaktformular."
-                    button-text="Kontakt" button-bg="bg-darkPrimary"/>
+      <!--Stats-->
+      <div class="flex space-x-2 justify-center md:order-1">
+        <span class="text-lg text-accent font-bold w-fit bg-red-200">Hello</span>
+        <span class="text-lg text-accent font-bold w-fit bg-red-200">World</span>
+        <span class="text-lg text-accent font-bold w-fit bg-red-200">Oida</span>
       </div>
-    </section>
+      <!--Buttons-->
+      <div class="flex justify-center space-x-4 md:order-3 md:flex-col md:items-center">
+            <span class="px-4 py-2 text-darkPrimary
+                      hover:text-accent hover:cursor-pointer">Termin vereinbaren
+            </span>
+        <span class="px-4 py-2 bg-accent text-white rounded-xl w-fit
+                      hover:text-darkPrimary hover:cursor-pointer">Nachricht senden
+            </span>
+      </div>
 
-    <!--Footer-->
-    <section>
-      <FooterBar/>
-    </section>
 
   </div>
+
+      <!--BodySection-Container-->
+      <div class="w-full min-h-screen bg-darkPrimary">Hello oida</div>
+
+
+
+
+  </div>
+
+  <!--Topsection-->
+  <!--  Image-->
+  <!--  Stats-->
+  <!--  Buttons-->
+
+  <!--Body-->
 </template>
 
 <script setup>
-import CardNormal from "@/components/util/elements/CardNormal.vue";
-import NavBar from "@/components/global/NavBar.vue";
-import HeroImage from "@/components/home/HeroImage.vue";
-import FeatureBlock from "@/components/home/FeatureBlock.vue";
-import TestimonialHorizontal from "@/components/util/elements/TestimonialHorizontal.vue";
-import TimeLine from "@/components/home/TimeLine.vue";
-import FooterBar from "@/components/global/FooterBar.vue";
-import LoginModal from "@/components/authentification/LoginModal.vue";
-import {ref} from "vue";
-import RegisterModal from "@/components/authentification/RegisterModal.vue";
-
-
-
-const registerOpen = ref(false)
-const loginOpen = ref(false)
-
-const openRegisterModal = () => {
-  registerOpen.value = true
-}
-
-const openLoginModal = () => {
-  console.log('open')
-  loginOpen.value = true
-  console.log(loginOpen.value)
-}
 
 </script>
 
