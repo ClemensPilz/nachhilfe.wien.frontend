aadcb5a2b4524bef509426c1bcc1bb796a81ca61
Clemens Pilz <office@clemenspilz.com>
2023-06-12 17:51:09 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,21 @@
 
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import {onMounted} from "vue";
+
+
+async function initApp() {
+  console.log('init');
+  if (localStorage.getItem('userId')) {
+    const userId = localStorage.getItem('userId');
+
+    //use Token to authentificate
+    //save Enums in appStore and userInformation in userStore
+
+  }
+}
+
+onMounted(() => initApp());
 
 </script>
 
