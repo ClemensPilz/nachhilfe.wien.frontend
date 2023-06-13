0592462147745788e608277726596af6de35cce3
Clemens Pilz <office@clemenspilz.com>
2023-06-13 09:25:23 +0200

    refined searchresult

--- src/components/search/SearchResult.vue
+++ src/components/search/SearchResult.vue
@@ -1,25 +1,41 @@
 <template>
 
-  <div class="grid grid-cols-3 grid-rows-1 bg-darkPrimary">
+  <div class="my-4 grid grid-cols-3 grid-rows-1 bg-darkPrimary rounded-xl shadow-lg">
     <!--Text-Part-->
     <div class="col-span-2 text-lightPrimary flex flex-col justify-center p-4">
-      <div class="text-xl text-accent">Hansi Huber</div>
-      <div>Hello Teacher</div>
-      <ButtonAccent text="Contact" class="w-fit my-3" />
+      <div class="text-xl text-accent">{{ name }}</div>
+      <div class="text-sm">{{ description }}</div>
+
+      <div id="divider" class="border border-b-1 border-lightPrimary mt-2 mb-3"></div>
+
+      <div class="flex gap-2 flex-wrap text-xs">
+        <div v-for="coaching in coachings"
+             class="px-2 py-1 rounded-lg border border-white
+              hover:border-accent hover:cursor-pointer select-none">
+          {{ coaching.subject }}: {{ coaching.price }}
+        </div>
+      </div>
+      <ButtonAccent text="Contact" class="w-fit my-3"/>
     </div>
-    
+
     <!--Image-Part-->
     <div class="col-span-1">
       <img src="https://placehold.co/200x250" alt="Image of a teacher" class="w-full">
     </div>
   </div>
-  
-  
+
+
 </template>
 
 <script setup>
-
 import ButtonAccent from "@/components/util/elements/ButtonAccent.vue";
+
+const props = defineProps({
+  'name': String,
+  'description': String,
+  'rating': String,
+  'coachings': Array
+})
 </script>
 
 <style lang="scss" scoped>
--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -2,8 +2,23 @@
   <div class="container mx-auto max-w-6xl">
     <nav-bar />
 
-    <div class="container min-h-screen max-w-lg mx-auto flex flex-col justify-center">
-        <SearchResult />
+    <div class="container min-h-screen max-w-lg mx-auto flex flex-col justify-center mt-4">
+        <SearchResult
+          name="Hansi Huber"
+          description="Student in Wien, will etwas dazuverdienen und mein Wissen weitergeben"
+          :coachings="[
+              {
+                'subject': 'Mathematik',
+                'price': '19 €'
+              },
+              {
+                'subject': 'Deutsch',
+                'price': '17 €'
+              }
+          ]"
+
+
+        /><SearchResult /><SearchResult /><SearchResult /><SearchResult />
     </div>
   </div>
 </template>
