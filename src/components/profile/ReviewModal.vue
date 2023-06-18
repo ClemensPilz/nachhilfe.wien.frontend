c873a82866de2e082d31bcfb5980771a2406275e
Clemens Pilz <office@clemenspilz.com>
2023-06-18 21:26:18 +0200

    you can now send reviews from profileView

--- src/components/profile/ReviewModal.vue
+++ src/components/profile/ReviewModal.vue
@@ -1,108 +1,150 @@
 <template>
-  <div v-if="props.isOpen" @click="closeModal" @keydown.enter="login"
-       class="fixed inset-0 flex items-center justify-center z-[1001] bg-black bg-opacity-50">
-    <div @click.stop="" class="max-h-screen bg-white py-6 px-10 mx-2 mt-20 rounded-xl">
+  <div
+    v-if="props.isOpen"
+    @click="closeModal"
+    @keydown.enter="login"
+    class="fixed inset-0 flex items-center justify-center z-[1001] bg-black bg-opacity-50"
+  >
+    <div
+      @click.stop=""
+      class="max-h-screen bg-white py-6 px-10 mx-2 mt-20 rounded-xl"
+    >
       <h2 class="text-2xl mb-4">{{ title }}</h2>
       <div>
-        <slot/>
+        <slot />
 
         <form>
           <RatingSelect v-model="rating" :options="['1', '2', '3', '4', '5']" />
 
           <label for="text">Review:</label>
-          <input type="text" name="text" v-model="text" placeholder="Mein Review...">
+          <input
+            type="text"
+            name="text"
+            v-model="text"
+            placeholder="Mein Review..."
+          />
           <div class="error">{{ errors.text }}</div>
         </form>
       </div>
 
       <!--Buttons-->
       <div class="flex gap-2 justify-end">
-        <button class="mt-4 w-fit py-2 px-4 bg-lightPrimary text-white rounded-xl" @click="closeModal">Schließen</button>
-        <button class="mt-4 w-fit py-2 px-4 bg-accent text-white rounded-xl" @click="login">Absenden</button>
+        <button
+          class="mt-4 w-fit py-2 px-4 bg-lightPrimary text-white rounded-xl"
+          @click="closeModal"
+        >
+          Schließen
+        </button>
+        <button
+          class="mt-4 w-fit py-2 px-4 bg-accent text-white rounded-xl"
+          @click="postReview"
+        >
+          Absenden
+        </button>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import {computed, ref} from 'vue'
-import {useForm} from "vee-validate";
-import {useUserStore} from "@/stores/user";
-import {useRouter} from "vue-router";
+import { computed, ref } from "vue";
+import { useForm } from "vee-validate";
+import { useUserStore } from "@/stores/user";
+import {useRoute, useRouter} from "vue-router";
 import RatingSelect from "@/components/profile/RatingSelect.vue";
+import axios from "axios";
 
 const userStore = useUserStore();
-const user = computed(() => userStore.user)
+const user = computed(() => userStore.user);
 const router = useRouter();
+const route = useRoute();
 
 const props = defineProps({
   title: {
     type: String,
-    required: true
+    required: true,
   },
   isOpen: {
-    type: Boolean
+    type: Boolean,
+  },
+  teacherId: {
+    type: String
   }
-})
+});
 
-const emit = defineEmits(['update:reviewModalOpen'])
+const emit = defineEmits(["update:reviewModalOpen"]);
 
 const closeModal = () => {
-  emit('update:reviewModalOpen', false)
-}
+  emit("update:reviewModalOpen", false);
+};
 
 // Validation
 const validationSchema = {
   rating() {
-    return true
+    return true;
   },
 
   text(value) {
     if (value.trim().length < 8) {
-      return "Bitte Text eingeben"
+      return "Bitte Text eingeben";
     }
     return true;
-  }
-}
+  },
+};
 
-const {meta, errors, useFieldModel} = useForm({
+const { meta, errors, useFieldModel } = useForm({
   validationSchema: validationSchema,
   initialValues: {
-    rating: '3',
-    text: ''
-  }
-})
+    rating: "1",
+    text: "",
+  },
+});
 
 const [rating, text] = useFieldModel(['rating', 'text']);
 
+async function postReview() {
+  if (meta.value.valid) {
+    try {
+      let date = new Date();
+      date = date.toISOString().slice(0,10);
 
-//Rest-Request
-
-const type = ref('Student');
-
-async function login() {
-  try {
-    const response = await userStore.auth({'email': email.value, 'password': password.value});
-    if (response.status === 1) {
-      await router.push(`/profile/${response.data.userId}`);
+      const response = await axios({
+        headers: {
+          "Authorization": `Bearer ${localStorage.getItem('token')}`
+        },
+        method: "post",
+        url: `${userStore.url}/feedback`,
+        data: {
+          "teacherId": route.params.userId,
+          "studentId": userStore.userId,
+          "title": "title",
+          "content": text.value,
+          "rating": rating.value,
+          "date": date
+        }
+      });
+      console.log(response.data);
+    } catch (e) {
+      console.log(e)
     }
-  } catch (e) {
-    console.log(e);
+
+    console.log("valid!");
+  } else {
+    console.log("invalid");
   }
 }
-
 </script>
 
 <style scoped>
 label {
-  @apply text-primary text-xs
+  @apply text-primary text-xs;
 }
 
 input {
-  @apply text-gray-200 m-2 py-1 px-2 rounded-lg focus:outline-none bg-gray-500 appearance-none
+  @apply text-gray-200 m-2 py-1 px-2 rounded-lg focus:outline-none bg-gray-500 appearance-none;
 }
 
 .error {
-  @apply text-xs text-red-600 italic
+  @apply text-xs text-red-600 italic;
 }
 </style>
--- src/stores/user.js
+++ src/stores/user.js
@@ -1,73 +1,64 @@
-import {ref, computed} from 'vue'
-import {defineStore} from 'pinia'
-import {useRouter} from "vue-router";
+import { ref, computed } from "vue";
+import { defineStore } from "pinia";
+import { useRouter } from "vue-router";
 import axios from "axios";
 
-export const useUserStore = defineStore('user', () => {
+export const useUserStore = defineStore("user", () => {
+  const url = "http://localhost:8080";
+  const router = useRouter();
+  const user = ref({});
+  const storeUserId = ref();
+  const userId = computed(() => storeUserId.value);
+  const isAuthenticated = ref(false);
 
-    const url = 'http://localhost:8080'
-    const router = useRouter();
-    const user = ref({});
-    const storeUserId = ref();
-    const userId = computed(() => storeUserId.value);
-    const isAuthenticated = ref(false);
+  async function tokenAuth(token) {}
 
-    async function tokenAuth(token) {
+  //Saves userId to localStorage, userinfos to store and returns 1 if successful, -1 if not
+  async function auth(credentials) {
+    let axiosConfig;
 
+    if (credentials.email) {
+      axiosConfig = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "post",
+        url: `${url}/auth`,
+        data: {
+          email: credentials.email,
+          password: credentials.password,
+        },
+      };
+    } else if (credentials.token) {
+      axiosConfig = {
+        headers: {
+          Authorization: "Bearer " + credentials.token,
+        },
+        method: "post",
+        url: `${url}/auth/info`,
+      };
+    } else {
+      return { status: -1, error: "Insufficient credentials" };
     }
 
-//Saves userId to localStorage, userinfos to store and returns 1 if successful, -1 if not
-    async function auth(credentials) {
-        let axiosConfig;
-
-        if (credentials.email) {
-            axiosConfig = {
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                method: 'post',
-                url: `${url}/auth`,
-                data: {
-                    "email": credentials.email,
-                    "password": credentials.password
-                }
-            }
-
-        } else if (credentials.token) {
-          axiosConfig = {
-            headers: {
-              'Authorization': "Bearer " + credentials.token
-            },
-            method: 'post',
-            url: `${url}/auth/info`
-          }
-        }
-
-        else {
-            return {status: -1, error: 'Insufficient credentials'};
-        }
-
-        try {
-            const response = await axios(axiosConfig);
-            if (response.status === 200) {
-                console.log(response.data);
-                user.value = response.data;
-                storeUserId.value = response.data.userId;
-                localStorage.setItem('token', response.data.token);
-                localStorage.setItem("userId", response.data.userId);
-                isAuthenticated.value = true;
-                return {status: 1, data: response.data};
-
-            } else {
-                return {status: -1, error: 'Unexpected response status'};
-            }
-
-        } catch (e) {
-            console.error(e);
-            return {status: -1, error: e};
-        }
+    try {
+      const response = await axios(axiosConfig);
+      if (response.status === 200) {
+        console.log(response.data);
+        user.value = response.data;
+        storeUserId.value = response.data.userId;
+        localStorage.setItem("token", response.data.token);
+        localStorage.setItem("userId", response.data.userId);
+        isAuthenticated.value = true;
+        return { status: 1, data: response.data };
+      } else {
+        return { status: -1, error: "Unexpected response status" };
+      }
+    } catch (e) {
+      console.error(e);
+      return { status: -1, error: e };
     }
+  }
 
-
-    return {userId, user, url, auth, isAuthenticated}
-})
+  return { userId, user, url, auth, isAuthenticated };
+});
