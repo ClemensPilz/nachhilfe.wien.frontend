bf76307c6efd94b67df7cf74143b59c97e4ad72f
Clemens Pilz <office@clemenspilz.com>
2023-06-19 21:53:08 +0200

    implemented function to fetch all teachers

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container mx-auto max-w-6xl">
-    <nav-bar />
+    <nav-bar/>
 
     <div class="container min-h-screen max-w-lg mx-auto flex flex-col justify-center mt-4">
-        <SearchResult
+      <SearchResult
           name="Hansi Huber"
           description="Student in Wien, will etwas dazuverdienen und mein Wissen weitergeben"
           :coachings="[
@@ -18,7 +18,11 @@
           ]"
 
 
-        /><SearchResult /><SearchResult /><SearchResult /><SearchResult />
+      />
+      <SearchResult/>
+      <SearchResult/>
+      <SearchResult/>
+      <SearchResult/>
     </div>
   </div>
 </template>
@@ -27,6 +31,42 @@
 
 import SearchResult from "@/components/search/SearchResult.vue";
 import NavBar from "@/components/global/NavBar.vue";
+import {onMounted, ref, watch} from "vue";
+import {useUserStore} from "@/stores/user";
+import axios from "axios";
+
+const userStore = useUserStore();
+const teachersArray = ref();
+
+async function getAllTeachers() {
+  try {
+    const response = await axios({
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('token')}`
+      },
+      method: 'GET',
+      url: `${userStore.url}/teacher/teacher-profiles`
+    });
+    teachersArray.value = response.data;
+    for (const teacher of teachersArray.value) {
+      console.log(teacher);
+    }
+
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+onMounted(async () => {
+  const unwatch = watch(() => userStore.isAuthenticated, async (newVal) => {
+    if (newVal) {
+      await getAllTeachers();
+      unwatch();
+    }
+  }, {immediate: true})
+})
+
+
 </script>
 
 <style lang="scss" scoped>
