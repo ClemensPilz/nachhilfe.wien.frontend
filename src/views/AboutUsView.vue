f42f1f8e097c21bfbed916eb131683abccd049b4
René-Ali Günürün <renee.guenueruen@gmail.com>
2023-06-21 21:29:52 +0200

    about us page video implemented

--- src/views/AboutUsView.vue
+++ src/views/AboutUsView.vue
@@ -7,54 +7,56 @@
       <div class="2xl:container 2xl:mx-auto lg:py-16 lg:px-20 md:py-12 md:px-6 py-9 px-4">
         <div class="flex flex-col lg:flex-row justify-between gap-8">
           <div class="w-full lg:w-5/12 flex flex-col justify-center">
-            <h1 class="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 dark:text-white pb-4">About Us</h1>
-            <p class="font-normal text-base leading-6 text-gray-600 dark:text-white">It is a long established fact that
-              a reader will be distracted by the readable content of a page when looking at its layout. The point of
-              using Lorem Ipsum.In the first place we have granted to God, and by this our present charter confirmed for
-              us and our heirs forever that the English Church shall be free, and shall have her rights entire, and her
-              liberties inviolate; and we will that it be thus observed; which is apparent from</p>
+            <h1 class="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 dark:text-white pb-4">Unsere Mission</h1>
+            <p class="font-normal text-base leading-6 text-gray-600 dark:text-white">
+              Unsere Mission als Nachhilfeplattform besteht darin, Bildung und Wissen zugänglicher zu machen und
+              Schülern dabei zu helfen, ihr volles Potenzial auszuschöpfen. Unser Ziel ist es, eine sichere und
+              vertrauenswürdige Plattform zu schaffen, auf der SchülerInnen auf erfahrene und kompetenete
+              NachhilfelehrerInnen treffen können.</p> <br>
+            <p class="font-bold text-mainBlue">Einfach gemeinsam besser lernen!</p>
+
           </div>
           <div class="w-full lg:w-8/12">
-            <img class="w-full h-full" src="https://i.ibb.co/FhgPJt8/Rectangle-116.png" alt="A group of People"/>
+            <video
+                src="src/assets/videos/pexels-pressmaster-3209298-1920x1080-25fps.mp4"
+                autoplay loop class="w-full h-auto">
+              Your browser does not support the video tag.
+              >
+            </video>
           </div>
         </div>
 
         <div class="flex lg:flex-row flex-col justify-between gap-8 pt-12">
           <div class="w-full lg:w-5/12 flex flex-col justify-center">
-            <h1 class="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 dark:text-white pb-4">Our Story</h1>
-            <p class="font-normal text-base leading-6 text-gray-600 dark:text-white">It is a long established fact that
-              a reader will be distracted by the readable content of a page when looking at its layout. The point of
-              using Lorem Ipsum.In the first place we have granted to God, and by this our present charter confirmed for
-              us and our heirs forever that the English Church shall be free, and shall have her rights entire, and her
-              liberties inviolate; and we will that it be thus observed; which is apparent from</p>
+            <h1 class="text-3xl lg:text-4xl font-bold leading-9 text-mainBlue-800 dark:text-white pb-4">Unser
+              Team</h1>
+            <p class="font-normal text-base leading-6 text-gray-600 dark:text-white">Unser Team besteht aus drei
+              motivierten und wissensdurstigen Entwicklern, die es sich zum Ziel gemacht hat Wissen auf einfache Weise
+              zu vermitteln. Denn wie es so schön heißt </p> <br>
+            <p class="font-bold text-mainBlue">"Mit dem Wissen ist es wie mit der
+              Liebe:
+              Je mehr man sie teilt, desto größer wird sie." </p>
           </div>
-          <div class="w-full lg:w-8/12 lg:pt-8">
+          <div class="w-full lg:w-8/12 lg:pt-8 pl-10">
             <div class="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 lg:gap-4 shadow-lg rounded-md">
               <div class="p-4 pb-6 flex justify-center flex-col items-center">
                 <img class="md:block hidden" src="https://i.ibb.co/FYTKDG6/Rectangle-118-2.png"
                      alt="Alexa featured Image"/>
                 <img class="md:hidden block" src="https://i.ibb.co/zHjXqg4/Rectangle-118.png"
                      alt="Alexa featured Image"/>
-                <p class="font-medium text-xl leading-5 text-gray-800 dark:text-white mt-4">Alexa</p>
+                <p class="font-medium text-xl leading-5 text-gray-800 dark:text-white mt-4">Dominik</p>
               </div>
               <div class="p-4 pb-6 flex justify-center flex-col items-center">
                 <img class="md:block hidden" src="https://i.ibb.co/fGmxhVy/Rectangle-119.png"
                      alt="Olivia featured Image"/>
                 <img class="md:hidden block" src="https://i.ibb.co/NrWKJ1M/Rectangle-119.png"
                      alt="Olivia featured Image"/>
-                <p class="font-medium text-xl leading-5 text-gray-800 dark:text-white mt-4">Olivia</p>
+                <p class="font-medium text-xl leading-5 text-gray-800 dark:text-white mt-4">Clemens</p>
               </div>
               <div class="p-4 pb-6 flex justify-center flex-col items-center">
                 <img class="md:block hidden" src="https://i.ibb.co/Pc6XVVC/Rectangle-120.png" alt="Liam featued Image"/>
                 <img class="md:hidden block" src="https://i.ibb.co/C5MMBcs/Rectangle-120.png" alt="Liam featued Image"/>
-                <p class="font-medium text-xl leading-5 text-gray-800 dark:text-white mt-4">Liam</p>
-              </div>
-              <div class="p-4 pb-6 flex justify-center flex-col items-center">
-                <img class="md:block hidden" src="https://i.ibb.co/7nSJPXQ/Rectangle-121.png"
-                     alt="Elijah featured image"/>
-                <img class="md:hidden block" src="https://i.ibb.co/ThZBWxH/Rectangle-121.png"
-                     alt="Elijah featured image"/>
-                <p class="font-medium text-xl leading-5 text-gray-800 dark:text-white mt-4">Elijah</p>
+                <p class="font-medium text-xl leading-5 text-gray-800 dark:text-white mt-4">René</p>
               </div>
             </div>
           </div>
@@ -104,4 +106,5 @@
 <script setup>
 import NavBar from "@/components/global/NavBar.vue";
 import FooterBar from "@/components/global/FooterBar.vue";
+
 </script>
