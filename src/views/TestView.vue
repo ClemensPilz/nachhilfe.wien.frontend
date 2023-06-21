2516d27ebdcfba028c99731c19123b0f6446887c
Clemens Pilz <office@clemenspilzcom>
2023-06-21 23:07:30 +0200

    fixed call to action in heroImage and startet experimenting with base64

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -4,86 +4,83 @@
   <div class="container max-w-6xl mx-auto text-center bg-white">
     <NavBar class="md:mb-12"/>
 
-    <!--Programmatic-->
-    <div v-if="props.profile">
+    <h1 class="text-2xl font-bold text-primary mb-4">Welche Fächer bietest du an?</h1>
+    <ButtonAccent text="Fach hinzufügen" @click="openCoachingForm" />
 
-      <!--TopSection-Container-->
-      <div class="md:grid md:grid-cols-3 grid-cols-1 gap-4 items-center">
-
-        <!--Image-->
-        <div class="order-1 md:order-2 flex justify-center">
-          <img class="w-full md:w-48 md:rounded-full shadow-xl"
-               src="https://images.unsplash.com/photo-1601233749202-95d04d5b3c00?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=838&q=80"
-               alt="profile picture of a funny guy">
-        </div>
-
-        <!--Stats-->
-        <div class="order-2 md:order-1 flex flex-col md:flex-row md:justify-center space-y-4 md:space-y-0 md:space-x-6">
-
-          <div class="flex flex-col items-center">
-            <div class="text-2xl font-bold text-accent">22</div>
-            <div class="text-lg text-primary w-fit">Kommentare</div>
-          </div>
-
-          <div class="flex flex-col items-center">
-            <div class="text-2xl font-bold text-accent">{{ props.profile.profile.averageRatingScore }}</div>
-            <div class="text-lg text-primary w-fit">Bewertung</div>
-          </div>
-
-        </div>
-
-        <!--Buttons-->
-        <div class="order-3 md:order-3 flex flex-col md:flex-row md:justify-center space-y-4 md:space-y-0 md:space-x-6">
-          <ButtonPrimary text="Button Primary"/>
-          <ButtonSecondary text="Button Secondary"/>
-          <ButtonAccent text="Button Accent"/>
-        </div>
+    <div class="grid grid-rows-2 grid-cols-4 mt-4">
+      <div class="col-span-4 md:col-span-2 bg-blue-400">
+        <!--Available subjects-->
 
       </div>
+      <div class="col-span-4 md:col-span-2 bg-red-400">
+        <!--Current Subject-->
 
-
-      <!--BodySection-Container-->
-      <div
-          class="block rounded-lg bg-white text-center shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]">
-        <div
-            class="border-b-2 border-neutral-100 px-6 py-3">
-          {{ props.profile.firstName }} {{ props.profile.lastname }}
-        </div>
-        <div class="p-6">
-          <h5
-              class="mb-2 headline">
-            {{ props.profile.profile.userName }}
-          </h5>
-          <p class="mb-4 paragraph">
-            {{ props.profile.profile.description }}
-          </p>
-
-          <ButtonAccent text="Nachricht senden"></ButtonAccent>
-
-        </div>
-        <div
-            class="border-t-2 border-neutral-100 px-6 py-3 dark:border-neutral-600 dark:text-neutral-50">
-          2 days ago
-        </div>
       </div>
-
     </div>
-  </div>
 
+    <input type="file" name="imageInput" id="imageInput">
+    <ButtonPrimary text="Encode" @click="encodeImageToBase64" />
+
+    <ButtonAccent text="Decode" @click="decodeImageFromBase64" />
+    <div id="imageOutput" class="max-w-2xl overflow-hidden"></div>
+
+    <div>{{base64}}</div>
+
+  </div>
 </template>
 
 <script setup>
 
 import NavBar from '@/components/global/NavBar.vue'
-import ButtonPrimary from '@/components/util/elements/ButtonPrimary.vue'
 import { Ripple, initTE } from "tw-elements";
-import {onMounted} from "vue";
-import ButtonSecondary from "@/components/util/elements/ButtonSecondary.vue";
+import {computed, onMounted, ref, watch} from "vue";
 import ButtonAccent from "@/components/util/elements/ButtonAccent.vue";
+import axios from "axios";
+import {useUserStore} from "@/stores/user";
+import {useAppStore} from "@/stores/app";
+import ButtonPrimary from "@/components/util/elements/ButtonPrimary.vue";
 
-const props = defineProps({
-  "profile": Object
-});
+const subject = ref();
+const level = ref();
+const rate = ref();
+const userStore = useUserStore();
+const appStore = useAppStore();
+const image = ref();
+const base64 = ref();
+
+function openCoachingForm() {
+
+}
+
+async function encodeImageToBase64() {
+  const imageData = document.getElementById('imageInput').files[0];
+  const reader = new FileReader();
+  let baseString;
+  reader.onloadend = () => {
+    baseString = reader.result;
+    base64.value = baseString;
+    console.log(baseString);
+  };
+  reader.readAsDataURL(imageData);
+}
+
+
+async function offerCoaching(newCoachingsArray) {
+  try {
+    const response = await axios({
+      headers: {
+        "Authorization": `Bearer ${localStorage.getItem('token')}`
+      },
+      url: `${userStore.url}/coaching/offer-coaching/${userStore.userId}`,
+      method: "post",
+      data: {
+        "coachings": newCoachingsArray
+      }
+    })
+  } catch (e) {
+    console.log('Error trying to post new coachings: ' + e.toString());
+  }
+}
 
 onMounted(() => initTE({Ripple}));
 </script>
