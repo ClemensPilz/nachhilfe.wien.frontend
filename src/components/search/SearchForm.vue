8cff5c336cd8bcd784d07cf98e5d9a2117cfb08e
Clemens Pilz <office@clemenspilz.com>
2023-07-09 18:17:49 +0200

    added another function to search

--- src/components/search/SearchForm.vue
+++ src/components/search/SearchForm.vue
@@ -2,12 +2,17 @@
   <form>
 
     <!--District-->
+    <div v-for="n in 23" class="inline">
+      <input type="checkbox" :id="`district_${n}`" :name="`district_${n}`" @change="log"
+             :value="n < 10 ? `DISTRICT_10${n}0` : `DISTRICT_1${n}0`" v-model="selectedDistricts">
+      <label>Bezirk {{ n }}</label>
+    </div>
+
+    <!--Subject-->
     <div v-for="subject in appStore.subjects" :key="subject" @change="log">
       <label :for="subject">{{ subject }}</label>
       <input type="checkbox" :name="subject" :id="subject" :value="subject" v-model="selectedSubjects">
     </div>
-
-    <!--Subject-->
 
     <!--Minrate/Maxrate-->
     <label for="maxRate">Höchster Stundensatz</label>
@@ -20,11 +25,14 @@
 <script setup>
 import {useAppStore} from "@/stores/app";
 import {ref} from "vue";
+
 const appStore = useAppStore();
 const selectedSubjects = ref([]);
+const selectedDistricts = ref([]);
 
 function log() {
   console.log(selectedSubjects.value);
+  console.log(selectedDistricts.value);
 }
 
 </script>
