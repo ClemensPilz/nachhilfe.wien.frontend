19569fbed5c69ecd7763443e9e1a44b1132c4418
Clemens Pilz <office@clemenspilzcom>
2023-07-15 08:50:26 +0200

    updated image for admin in dashboard

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -6,10 +6,10 @@
 
     <DashboardFeature
         v-if="userStore.user.userType === 'ADMIN'"
-        title="Admin-Funktionen" :left="false" subtitle="Nachhilfe.wien verwalten" text="Zu allen Admin-Funktionen" class="bg-mainOrange">
+        title="Admin-Funktionen" :left="true" subtitle="Nachhilfe.wien verwalten" text="Zu allen Admin-Funktionen" class="bg-mainOrange">
       <ButtonLarge class="bg-mainBlue" @click="() => {router.push('/admin')}" text="Admin" />
       <template v-slot:image>
-        <img src="@/assets/images/dashboard/message.jpg" alt="">
+        <img src="@/assets/images/dashboard/admin.jpg" alt="">
       </template>
     </DashboardFeature>
 
