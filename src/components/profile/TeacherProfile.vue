e0590cdaed49eb71c7104a285794c9fb9876284d
Clemens Pilz <clemensrochus.pilz@gmail.com>
2023-07-16 09:54:10 +0200

    Component can now show Student's profile too

--- src/components/profile/TeacherProfile.vue
+++ src/components/profile/TeacherProfile.vue
@@ -1,9 +1,10 @@
 <template>
+
   <div class="min-h-screen">
     <FormModal ref="reviewModalRef">
       <CardLarge class="m-0">
         <template v-slot:content>
-          <ReviewForm @close="closeReviewModal" :teacherId="teacherId" :studentId="userStore.user.userId"/>
+          <ReviewForm @close="closeReviewModal" :teacherId="userId" :studentId="userStore.user.userId"/>
         </template>
       </CardLarge>
     </FormModal>
@@ -18,98 +19,102 @@
     </FormModal>
 
     <div v-if="loaded">
-
       <h3 v-if="!profile" class="text-center">
         User nicht gefunden!
       </h3>
 
-      <section v-else id="teacherProfile">
+        <section v-else id="userProfile">
+      <div v-if="userType === 'STUDENT'">
+        Hello Student
+      </div>
 
-        <!--Container for content-->
-        <div class="container min-h-screen max-w-7xl mx-auto px-2 mt-8">
+      <div v-else>
 
-          <!--Grid-Layout-->
-          <div class="w-full grid grid-cols-2 gap-4 items-center">
+          <!--Container for content-->
+          <div class="container min-h-screen max-w-7xl mx-auto px-2 mt-8">
 
-            <!--Picture-->
-            <div id="teacherPicture" class="group">
+            <!--Grid-Layout-->
+            <div class="w-full grid grid-cols-2 gap-4 items-center">
 
-              <div class="w-full h-[500px] pl-16">
-                <img :src="profile.image ? profile.image : stockPhoto" alt="picture of a teacher"
-                     class="h-full object-cover object-center group-hover:scale-105 transition-all duration-300">
+              <!--Picture-->
+              <div id="teacherPicture" class="group">
+
+                <div class="w-full h-[500px] pl-16">
+                  <img :src="profile.image ? profile.image : stockPhoto" alt="picture of a teacher"
+                       class="h-full object-cover object-center group-hover:scale-105 transition-all duration-300">
+                </div>
+
+              </div>
+
+              <!--Information-->
+              <div id="teacherInformation" class="col-span-2 md:col-span-1">
+                <ul>
+                  <li>
+                    <h1 class="text-mainOrange">?</h1>
+                    <div>
+                      <h3>{{ profile.firstName }} {{ profile.lastName }}</h3>
+                      <p>{{ profile.description }}</p>
+                      <small>Durchschnittl. Wertung: {{ profile.averageRatingScore }}</small>
+
+                    </div>
+                  </li>
+                  <li>
+                    <h1> > </h1>
+                    <div>
+                      <div v-for="coaching in profile.coachings"
+                           v-show="coaching.active">
+                        <p class="underline underline-offset-4 decoration-secondary"
+                           @click="openAppointmentModal(coaching.coachingId)">{{ coaching.subject }}</p>
+                      </div>
+                      <div v-if="profile.coachings.length < 0">Dieser Lehrer bietet aktuell keine Nachhilfe an</div>
+                    </div>
+                  </li>
+                  <li>
+                    <h1>:</h1>
+                    <div>
+                      <small class="block pb-3">Lehrer kontaktieren:</small>
+                      <div class="flex gap-2 flex-wrap">
+                        <ButtonRegular class="bg-mainOrange" text="Nachricht"
+                                       @click="appStore.sendMessage(userId, true)"/>
+                        <ButtonRegular v-if="userStore.user.userType === 'STUDENT'" class="bg-mainBlue" text="Bewerten"
+                                       @click="openReviewModal"/>
+                        <ButtonRegular v-else class="bg-secondary" text="Bewerten" @click="noStudentAlert"/>
+                      </div>
+                    </div>
+
+                  </li>
+                </ul>
+
               </div>
 
             </div>
 
-            <!--Information-->
-            <div id="teacherInformation" class="col-span-2 md:col-span-1">
-              <ul>
-                <li>
-                  <h1 class="text-mainOrange">?</h1>
-                  <div>
-                    <h3>{{ profile.firstName }} {{ profile.lastName }}</h3>
-                    <p>{{ profile.description }}</p>
-                    <small>Durchschnittl. Wertung: {{ profile.averageRatingScore }}</small>
-
-                  </div>
-                </li>
-                <li>
-                  <h1> > </h1>
-                  <div>
-                    <div v-for="coaching in profile.coachings"
-                         v-show="coaching.active">
-                      <p class="underline underline-offset-4 decoration-secondary"
-                         @click="openAppointmentModal(coaching.coachingId)">{{ coaching.subject }}</p>
-                    </div>
-                    <div v-if="hasNoCoaching">Dieser Lehrer bietet aktuell keine Nachhilfe an</div>
-                  </div>
-                </li>
-                <li>
-                  <h1>:</h1>
-                  <div>
-                    <small class="block pb-3">Lehrer kontaktieren:</small>
-                    <div class="flex gap-2 flex-wrap">
-                      <ButtonRegular class="bg-mainOrange" text="Nachricht"
-                                     @click="appStore.sendMessage(teacherId, true)"/>
-                      <ButtonRegular v-if="userStore.user.userType === 'STUDENT'" class="bg-mainBlue" text="Bewerten"
-                                     @click="openReviewModal"/>
-                      <ButtonRegular v-else class="bg-secondary" text="Bewerten" @click="noStudentAlert"/>
-                    </div>
-                  </div>
-
-                </li>
-              </ul>
+            <!--Comments-->
+            <div class="col-span-2 my-4" v-if="profile.feedbacks.length > 0"> <!--@todo: check if user is teacher-->
+              <table class="w-full text-left text-p">
+                <tr>
+                  <th>Bewertung</th>
+                  <th>Stern</th>
+                  <th>von</th>
+                </tr>
+                <tr v-for="feedback in profile.feedbacks">
+                  <td>
+                    {{ feedback.content }}
+                  </td>
+                  <td>
+                    {{ feedback.rating }}
+                  </td>
+                  <td>
+                    {{ feedback.studentFirstName }}
+                  </td>
+                </tr>
+              </table>
 
             </div>
 
           </div>
-
-          <!--Comments-->
-          <div class="col-span-2 my-4" v-if="profile.feedbacks.length > 0"> <!--@todo: check if user is teacher-->
-            <table class="w-full text-left text-p">
-              <tr>
-                <th>Bewertung</th>
-                <th>Stern</th>
-                <th>von</th>
-              </tr>
-              <tr v-for="feedback in profile.feedbacks">
-                <td>
-                  {{ feedback.content }}
-                </td>
-                <td>
-                  {{ feedback.rating }}
-                </td>
-                <td>
-                  {{ feedback.studentFirstName }}
-                </td>
-              </tr>
-            </table>
-
-          </div>
-
-        </div>
-      </section>
-
+      </div>
+        </section>
     </div>
 
   </div>
@@ -133,14 +138,13 @@ const route = useRoute();
 const userStore = useUserStore();
 const appStore = useAppStore();
 const loaded = ref(false);
-const teacherId = route.params.userId;
+const userId = route.params.userId;
 const profile = ref(null);
+const userType = ref('');
 
 const reviewModalRef = ref();
 const appointmentModalRef = ref();
-const hasNoCoaching = computed(() => {
-  return profile.value.coachings.length < 1;
-})
+
 
 function openReviewModal() {
   reviewModalRef.value.openModal();
@@ -152,7 +156,7 @@ function closeReviewModal() {
 
 function openAppointmentModal(coachingId) {
   appointmentModalRef.value.openModal();
-  appStore.selectCoaching(teacherId, coachingId);
+  appStore.selectCoaching(userId, coachingId);
 }
 
 function closeAppointmentModal() {
@@ -163,24 +167,25 @@ function noStudentAlert() {
   alert('Nur Schüler können Bewertungen abgeben');
 }
 
-async function getTeacherProfile(id) {
+async function getUserProfile(id) {
   try {
     const response = await axios({
       headers: {
         "Authorization": `Bearer ${localStorage.getItem('token')}`
       },
       method: 'get',
-      url: `${userStore.url}/teacher/find-teacher/${id}`
+      url: `${userStore.url}/user/${id}`
     });
     profile.value = response.data;
+    userType.value = response.data.userType
     console.log(response.data)
   } catch (e) {
-    console.log('Error fetching teacher profile: ' + e);
+    console.log('Error fetching profile: ' + e);
   }
 }
 
 onBeforeMount(async () => {
-  await getTeacherProfile(teacherId);
+  await getUserProfile(userId);
   loaded.value = true;
 });
 
