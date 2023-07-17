c3fb4b4a559ed06f3ff3f9433ddc58b149174fd6
René-Ali Günürün <renee.guenueruen@gmail.com>
2023-07-17 21:34:17 +0200

    json inserted & bug has to be fixed

---
+++ src/components/aboutUs/Question.vue
@@ -0,0 +1,58 @@
+<script setup>
+
+const {question} = defineProps(["questions"]);
+
+
+</script>
+
+<template>
+  <div class="question-container">
+    <h1 class="question">
+      {{ questions.question }}
+    </h1>
+  </div>
+  <div class="options-container">
+    <div v-for="input in questions.input"
+         :key="input.id"
+         class="input">
+      <p class="option-label">{{ input.question }}</p>
+      <div class="option-value">
+        <p>{{input.answer}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.question-container {
+  margin-top: 20px;
+}
+
+.question {
+  font-size: 40px;
+  margin-bottom: 20px;
+}
+
+.option {
+  display: flex;
+  margin-bottom: 20px;
+  cursor: pointer;
+}
+
+.option-label {
+  background-color: bisque;
+  width: 50px;
+  height: 50px;
+  font-size: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.option-value {
+  background-color: rgb(244, 239, 239);
+  width: 100%;
+  font-size: 30px;
+  padding: 0 20px;
+}
+</style>
--- src/views/FaqView.vue
+++ src/views/FaqView.vue
@@ -1,3 +1,18 @@
+<script setup>
+import {ref, watch} from "vue";
+import q from "@/data/questions.json";
+import Question from "@/components/aboutUs/Question.vue";
+
+const search = ref("");
+const questions = ref(q);
+
+
+watch(search, () => {
+  questions.value = q.filter(question => question.name.toLowerCase().includes(search.value.toLowerCase()));
+      console.log("hallo")
+});
+</script>
+
 <template>
   <div class="container mx-auto max-w-6xl mt-8 px-2">
     <h2 class="text-mainBlue"> Du hast Fragen?</h2>
@@ -7,75 +22,80 @@
       könnten. Wenn du die gesuchte Antwort nicht finden solltest, kontaktiere uns einfach.</p>
   </div>
   <div class="container mx-auto max-w-6xl mt-8 px-2">
-    <h3> Allgemeine Fragen </h3>
-    <h4 class="mt-5"> 1. Was ist nachhilfe.wien?</h4>
-    <p class="mt-5"> nachhilfe.wien ist eine Online-Plattform, die Schülern in Wien und Umgebung Zugang zu qualitativ
-      hochwertiger
-      Nachhilfe bietet. Wir verbinden Schüler mit erfahrenen und geprüften Nachhilfelehrern in einer Vielzahl von
-      Fächern.</p>
+    <input v-model.trim="search" type="text" placeholder="Search...">
   </div>
-  <div class="container mx-auto max-w-6xl mt-8 px-2">
-    <h4 class="mt-5"> 2. Für wen ist diese Plattform gedacht?</h4>
-    <p class="mt-5"> Diese Plattform ist für Schüler aller Altersstufen und Bildungsniveaus konzipiert, die zusätzliche
-      Unterstützung in ihren Schulfächern benötigen.</p>
-  </div>
-  <div class="container mx-auto max-w-6xl mt-8 px-2">
-    <h3> Über die Nachhilfe </h3>
-    <h4 class="mt-5"> 3. In welchen Fächern wird Nachhilfe angeboten?</h4>
-    <p class="mt-5"> nWir bieten Nachhilfe in einer breiten Palette von Fächern an, darunter Mathematik, Deutsch,
-      Englisch, Physik, Chemie, Geschichte, und viele mehr.</p>
-  </div>
-  <div class="container mx-auto max-w-6xl mt-8 px-2">
-    <h4 class="mt-5"> 4. Wie werden Nachhilfelehrer ausgewählt?</h4>
-    <p class="mt-5"> Unsere Nachhilfelehrer werden sorgfältig ausgewählt und müssen einen strengen Prüfungs- und
-      Überprüfungsprozess durchlaufen. Wir legen großen Wert auf ihre fachliche Qualifikation, pädagogische Erfahrung
-      und Leidenschaft für das Lehren.</p>
-  </div>
-  <div class="container mx-auto max-w-6xl mt-8 px-2">
-    <h3> Buchung und Bezahlung </h3>
-    <h4 class="mt-5"> 5. Wie buche ich eine Nachhilfestunde?</h4>
-    <p class="mt-5"> Sie können eine Nachhilfestunde buchen, indem Sie das
-      gewünschte Fach und den gewünschten Lehrer auswählen und einen passenden Termin vereinbaren.</p>
-  </div>
-  <div class="container mx-auto max-w-6xl mt-8 px-2">
-    <h4 class="mt-5"> 6. Wie kann ich bezahlen?</h4>
-    <p class="mt-5"> Wir akzeptieren verschiedene Zahlungsmethoden, darunter Kreditkarte, PayPal und Banküberweisung.
-      Die Zahlung erfolgt sicher und verschlüsselt über unsere Plattform.</p>
-  </div>
-  <div class="container mx-auto max-w-6xl mt-8 px-2">
-    <h3> Technische Anforderungen </h3>
-    <h4 class="mt-5"> 7. Welche technischen Anforderungen gibt es?</h4>
-    <p class="mt-5"> Sie benötigen eine stabile Internetverbindung und einen Computer, ein Tablet oder ein Smartphone.
-      Unsere Plattform ist auf allen gängigen Betriebssystemen und Browsern zugänglich.</p>
-  </div>
-  <div class="container mx-auto max-w-6xl mt-8 px-2">
-    <h4 class="mt-5"> 8. Gibt es eine App?</h4>
-    <p class="mt-5"> Eine eigene App ist aktuell nicht angedacht. Wenn die Nachfrage jedoch größer wird und der Bedarf
-      vorhanden sein sollte, könnten wir über eine eigene App nachdenken.</p>
-  </div>
-  <div class="container mx-auto max-w-6xl mt-8 px-2">
-    <h3> Datenschutz </h3>
-    <h4 class="mt-5"> 9. Wie wird mit meinen Daten umgegangen?</h4>
-    <p class="mt-5"> Der Schutz Ihrer Daten ist uns sehr wichtig. Ihre persönlichen Daten werden streng vertraulich
-      behandelt und nicht an Dritte weitergegeben. Weitere Informationen finden Sie in unserer Datenschutzerklärung.</p>
-  </div>
-  <div class="container mx-auto max-w-6xl mt-8 px-2">
-    <h4 class="mt-5"> 10. Wie kann ich meinen Account löschen?</h4>
-    <p class="mt-5"> Um Ihren Account zu löschen, kontaktieren Sie bitte unseren Kundenservice. Nach der Bestätigung
-      wird Ihr Account und alle zugehörigen Daten permanent gelöscht.
+  <div class="questions-container">
+    <Question v-for="question in questions" :key="quesion.id" :question="question"/>
 
-      Sollten Sie weitere Fragen haben, die hier nicht beantwortet wurden, kontaktieren Sie uns bitte über unser
+
+<!--    <h3> Allgemeine Fragen </h3>-->
+<!--    <h4 class="mt-5"> 1. Was ist nachhilfe.wien?</h4>-->
+<!--    <p class="mt-5"> nachhilfe.wien ist eine Online-Plattform, die Schülern in Wien und Umgebung Zugang zu qualitativ-->
+<!--      hochwertiger-->
+<!--      Nachhilfe bietet. Wir verbinden Schüler mit erfahrenen und geprüften Nachhilfelehrern in einer Vielzahl von-->
+<!--      Fächern.</p>-->
+<!--  </div>-->
+<!--  <div class="container mx-auto max-w-6xl mt-8 px-2">-->
+<!--    <h4 class="mt-5"> 2. Für wen ist diese Plattform gedacht?</h4>-->
+<!--    <p class="mt-5"> Diese Plattform ist für Schüler aller Altersstufen und Bildungsniveaus konzipiert, die zusätzliche-->
+<!--      Unterstützung in ihren Schulfächern benötigen.</p>-->
+<!--  </div>-->
+<!--  <div class="container mx-auto max-w-6xl mt-8 px-2">-->
+<!--    <h3> Über die Nachhilfe </h3>-->
+<!--    <h4 class="mt-5"> 3. In welchen Fächern wird Nachhilfe angeboten?</h4>-->
+<!--    <p class="mt-5"> Wir bieten Nachhilfe in einer breiten Palette von Fächern an, darunter Mathematik, Deutsch,-->
+<!--      Englisch, Physik, Chemie, Geschichte, und viele mehr.</p>-->
+<!--  </div>-->
+<!--  <div class="container mx-auto max-w-6xl mt-8 px-2">-->
+<!--    <h4 class="mt-5"> 4. Wie werden Nachhilfelehrer ausgewählt?</h4>-->
+<!--    <p class="mt-5"> Unsere Nachhilfelehrer werden sorgfältig ausgewählt und müssen einen strengen Prüfungs- und-->
+<!--      Überprüfungsprozess durchlaufen. Wir legen großen Wert auf ihre fachliche Qualifikation, pädagogische Erfahrung-->
+<!--      und Leidenschaft für das Lehren.</p>-->
+<!--  </div>-->
+<!--  <div class="container mx-auto max-w-6xl mt-8 px-2">-->
+<!--    <h3> Buchung und Bezahlung </h3>-->
+<!--    <h4 class="mt-5"> 5. Wie buche ich eine Nachhilfestunde?</h4>-->
+<!--    <p class="mt-5"> Sie können eine Nachhilfestunde buchen, indem Sie das-->
+<!--      gewünschte Fach und den gewünschten Lehrer auswählen und einen passenden Termin vereinbaren.</p>-->
+<!--  </div>-->
+<!--  <div class="container mx-auto max-w-6xl mt-8 px-2">-->
+<!--    <h4 class="mt-5"> 6. Wie kann ich bezahlen?</h4>-->
+<!--    <p class="mt-5"> Wir akzeptieren verschiedene Zahlungsmethoden, darunter Kreditkarte, PayPal und Banküberweisung.-->
+<!--      Die Zahlung erfolgt sicher und verschlüsselt über unsere Plattform.</p>-->
+<!--  </div>-->
+<!--  <div class="container mx-auto max-w-6xl mt-8 px-2">-->
+<!--    <h3> Technische Anforderungen </h3>-->
+<!--    <h4 class="mt-5"> 7. Welche technischen Anforderungen gibt es?</h4>-->
+<!--    <p class="mt-5"> Sie benötigen eine stabile Internetverbindung und einen Computer, ein Tablet oder ein Smartphone.-->
+<!--      Unsere Plattform ist auf allen gängigen Betriebssystemen und Browsern zugänglich.</p>-->
+<!--  </div>-->
+<!--  <div class="container mx-auto max-w-6xl mt-8 px-2">-->
+<!--    <h4 class="mt-5"> 8. Gibt es eine App?</h4>-->
+<!--    <p class="mt-5"> Eine eigene App ist aktuell nicht angedacht. Wenn die Nachfrage jedoch größer wird und der Bedarf-->
+<!--      vorhanden sein sollte, könnten wir über eine eigene App nachdenken.</p>-->
+<!--  </div>-->
+<!--  <div class="container mx-auto max-w-6xl mt-8 px-2">-->
+<!--    <h3> Datenschutz </h3>-->
+<!--    <h4 class="mt-5"> 9. Wie wird mit meinen Daten umgegangen?</h4>-->
+<!--    <p class="mt-5"> Der Schutz Ihrer Daten ist uns sehr wichtig. Ihre persönlichen Daten werden streng vertraulich-->
+<!--      behandelt und nicht an Dritte weitergegeben. Weitere Informationen finden Sie in unserer Datenschutzerklärung.</p>-->
+<!--  </div>-->
+<!--  <div class="container mx-auto max-w-6xl mt-8 px-2">-->
+<!--    <h4 class="mt-5"> 10. Wie kann ich meinen Account löschen?</h4>-->
+<!--    <p class="mt-5"> Um Ihren Account zu löschen, kontaktieren Sie bitte unseren Kundenservice. Nach der Bestätigung-->
+<!--      wird Ihr Account und alle zugehörigen Daten permanent gelöscht.-->
+
+      <p>Sollten Sie weitere Fragen haben, die hier nicht beantwortet wurden, kontaktieren Sie uns bitte über unser
       Kontaktformular. Wir freuen uns, Ihnen weiterhelfen zu können!</p>
   </div>
 </template>
-<script setup>
-// import {ref} from "vue";
-//
-// const numberOfQuestions = ref(0);
-//
-// const countingQuestions = computed(() => {
-//   numberOfQuestions++
-// };
-// )
 
-</script>
+<style scoped>
+input {
+  border: none;
+  background-color: rgb(128, 128, 128, 0.1);
+  padding: 10px;
+  border-radius: 5px;
+}
+
+</style>
