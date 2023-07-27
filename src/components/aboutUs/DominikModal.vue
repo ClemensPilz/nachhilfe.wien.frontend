db5c93100fa88e9ef05dc05bc3e9c4032d9b1314
René-Ali Günürün <renee.guenueruen@gmail.com>
2023-07-27 22:48:43 +0200

    modals adapted

--- src/components/aboutUs/DominikModal.vue
+++ src/components/aboutUs/DominikModal.vue
@@ -1,45 +1,47 @@
 <template>
   <div
-    class="flex h-full w-full items-center justify-center rounded-xl bg-gray-100 shadow-lg"
+      class="flex h-full w-full items-center justify-center rounded-xl bg-gray-100 shadow-lg"
   >
     <transition name="modal-animation">
       <div
-        v-show="modalActive === 1"
-        class="fixed inset-0 z-50 flex items-center justify-center bg-gray-800 bg-opacity-70"
+          v-show="modalActive === 1"
+          class="fixed inset-0 z-50 flex items-center justify-center bg-gray-800 bg-opacity-70"
       >
         <div class="modal-inner w-[600px] rounded-lg bg-white p-8">
           <div
-            v-show="modalActive === 1"
-            class="modal-inner flex flex-col items-center p-8"
+              v-show="modalActive === 1"
+              class="modal-inner flex flex-col items-center p-8"
           >
             <h4 class="mb-4 text-center text-mainBlue">Das ist Dominik</h4>
             <div class="flex items-center justify-center">
               <img
-                class="mt-5 h-40 w-40 overflow-hidden rounded-full"
-                :src="dominikImage"
-                alt="Dominik Köberl"
+                  class="mt-5 h-40 w-40 overflow-hidden rounded-full"
+                  :src="dominikImage"
+                  alt="Dominik Köberl"
               />
             </div>
-            <p class="mt-5 text-center">
-              Dominik ist ein ambitionierter Entwickler, der sein Wissen gerne
-              teilt. Er strebt danach, anderen zu helfen, ihr volles Potenzial
-              auszuschöpfen. Neben seiner Arbeit liebt er es, in seiner Freizeit
-              Gitarre zu spielen. Dominik ist neugierig, immer auf dem neuesten
-              Stand der Technologie und bestrebt, kontinuierlich dazuzulernen.
-              Er ist ein talentierter Entwickler und ein großartiger
-              Kommunikator, der sein Wissen durch verschiedene Plattformen
-              teilt. Sein Ziel ist es, anderen zu helfen, ihre technischen
-              Fähigkeiten zu verbessern und ihre Ziele zu erreichen. Dominik
-              verkörpert die Eigenschaften eines engagierten und inspirierenden
-              Entwicklers, der sowohl in der Tech-Welt als auch in der Musik
-              Anerkennung findet.
-            </p>
-            <slot />
+            <div class="scrollable-content mt-5">
+              <p class="mt-5 text-center">
+                Dominik ist ein ambitionierter Entwickler, der sein Wissen gerne
+                teilt. Er strebt danach, anderen zu helfen, ihr volles Potenzial
+                auszuschöpfen. Neben seiner Arbeit liebt er es, in seiner Freizeit
+                Gitarre zu spielen. Dominik ist neugierig, immer auf dem neuesten
+                Stand der Technologie und bestrebt, kontinuierlich dazuzulernen.
+                Er ist ein talentierter Entwickler und ein großartiger
+                Kommunikator, der sein Wissen durch verschiedene Plattformen
+                teilt. Sein Ziel ist es, anderen zu helfen, ihre technischen
+                Fähigkeiten zu verbessern und ihre Ziele zu erreichen. Dominik
+                verkörpert die Eigenschaften eines engagierten und inspirierenden
+                Entwicklers, der sowohl in der Tech-Welt als auch in der Musik
+                Anerkennung findet.
+              </p>
+            </div>
+            <slot/>
             <ButtonRegular
-              text="Close"
-              class="mt-5 rounded bg-mainOrange px-4 py-2"
-              @click="closeModal"
-              type="button"
+                text="Close"
+                class="mt-5 rounded bg-mainOrange px-4 py-2"
+                @click="closeModal"
+                type="button"
             />
           </div>
         </div>
@@ -63,4 +65,11 @@ function closeModal() {
 console.log(props);
 </script>
 
-<style lang="less" scoped></style>
+<style lang="scss" scoped>
+
+.scrollable-content {
+  max-height:  400px;
+  overflow-y: scroll;
+}
+
+</style>
--- src/components/aboutUs/ReneModal.vue
+++ src/components/aboutUs/ReneModal.vue
@@ -1,46 +1,48 @@
 <template>
   <div
-    class="flex h-full w-full items-center justify-center rounded-xl bg-gray-100 shadow-lg"
+      class="flex h-full w-full items-center justify-center rounded-xl bg-gray-100 shadow-lg"
   >
     <transition name="modal-animation">
       <div
-        v-show="modalActive === 3"
-        class="fixed inset-0 z-50 flex items-center justify-center bg-gray-800 bg-opacity-70"
+          v-show="modalActive === 3"
+          class="fixed inset-0 z-50 flex items-center justify-center bg-gray-800 bg-opacity-70"
       >
         <div class="modal-inner w-[600px] rounded-lg bg-white p-8">
           <div
-            v-show="modalActive === 3"
-            class="modal-inner flex flex-col items-center p-8"
+              v-show="modalActive === 3"
+              class="modal-inner flex flex-col items-center p-8"
           >
             <h4 class="mb-4 text-center text-mainBlue">Das ist René</h4>
             <div class="flex items-center justify-center">
               <img
-                class="mt-5 h-40 w-40 overflow-hidden rounded-full"
-                :src="reneImage"
-                alt="Rene Ali Günürün"
+                  class="mt-5 h-40 w-40 overflow-hidden rounded-full"
+                  :src="reneImage"
+                  alt="Rene Ali Günürün"
               />
             </div>
-            <p class="mt-5 text-center">
-              René ist ein äußerst ambitionierter und leidenschaftlicher
-              Entwickler. Er betrachtet Wissen als kostbares Gut und teilt es
-              gerne mit anderen. Als Mentor unterstützt er aufstrebende
-              Entwickler und hilft ihnen, technische Herausforderungen zu
-              bewältigen. René strebt stets nach Verbesserung und bleibt auf dem
-              neuesten Stand der Technologie. Er ist neugierig,
-              experimentierfreudig und scheut sich nicht vor neuen
-              Herausforderungen. In seiner Freizeit arbeitet er an eigenen
-              Projekten und nutzt die Programmierung als Ausdruck seiner
-              Leidenschaft. René ist ein inspirierendes Vorbild für Entwickler,
-              die das Wissen schätzen und ihre Ambitionen nutzen, um innovative
-              Lösungen zu schaffen.
-            </p>
+            <div class="scrollable-content mt-5">
+              <p class="mt-5 text-center">
+                René ist ein äußerst ambitionierter und leidenschaftlicher
+                Entwickler. Er betrachtet Wissen als kostbares Gut und teilt es
+                gerne mit anderen. Als Mentor unterstützt er aufstrebende
+                Entwickler und hilft ihnen, technische Herausforderungen zu
+                bewältigen. René strebt stets nach Verbesserung und bleibt auf dem
+                neuesten Stand der Technologie. Er ist neugierig,
+                experimentierfreudig und scheut sich nicht vor neuen
+                Herausforderungen. In seiner Freizeit arbeitet er an eigenen
+                Projekten und nutzt die Programmierung als Ausdruck seiner
+                Leidenschaft. René ist ein inspirierendes Vorbild für Entwickler,
+                die das Wissen schätzen und ihre Ambitionen nutzen, um innovative
+                Lösungen zu schaffen.
+              </p>
+            </div>
 
-            <slot />
+            <slot/>
             <ButtonRegular
-              text="Close"
-              class="mt-5 rounded bg-mainOrange px-4 py-2"
-              @click="closeModal"
-              type="button"
+                text="Close"
+                class="mt-5 rounded bg-mainOrange px-4 py-2"
+                @click="closeModal"
+                type="button"
             />
           </div>
         </div>
@@ -53,7 +55,7 @@
 import ButtonRegular from "@/components/util/buttons/ButtonRegular.vue";
 import reneImage from "@/assets/images/team/rene.jpg";
 
-const { props } = defineProps(["modalActive"]);
+const {props} = defineProps(["modalActive"]);
 
 const emits = defineEmits(["close"]);
 
@@ -64,4 +66,11 @@ function closeModal() {
 console.log(props);
 </script>
 
-<style lang="less" scoped></style>
+<style lang="scss" scoped>
+
+.scrollable-content {
+  max-height: 400px;
+  overflow-y: scroll;
+}
+
+</style>
