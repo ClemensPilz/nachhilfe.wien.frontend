aee2f8d18d48a592bc2dee59b553eb8f65b39540
Dominik Köberl <dokoe92@gmail.com>
2023-07-27 20:41:57 +0200

    changed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,29 @@
-import { fileURLToPath, URL } from "node:url";
+import {fileURLToPath, URL} from "node:url";
 
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-  server: {
-    watch: {
-      onRestart() {
-        this.sendHmrMessage({ type: "full-reload" });
-      },
-    },
-  },
-});
+
+        plugins: [vue(), vueJsx()],
+        resolve: {
+            alias: {
+                "@": fileURLToPath(new URL("./src", import.meta.url)),
+            },
+        },
+        server: {
+            watch: {
+                onRestart() {
+                    this.sendHmrMessage({type: "full-reload"});
+                },
+            },
+        },
+        build: {
+            rollupOptions: {
+                external: ['src/assets/videos/pexels-pressmaster-3209298-1920x1080-25fps.mp4']
+            }
+        }
+    }
+);
